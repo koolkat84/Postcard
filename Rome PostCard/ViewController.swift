fe01fff1a726c3f4cb34da89b8ffe06d0a664778
koolkat84 <mrfelixog@gmail.com>
2014-12-13 13:13:22 +0200

    impleented name label

--- Rome PostCard/ViewController.swift
+++ Rome PostCard/ViewController.swift
@@ -11,13 +11,14 @@ import UIKit
 class ViewController: UIViewController {
     
     
+    @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var enterNameTextField: UITextField!
     @IBOutlet weak var enterMessageTextField: UITextField!
     
     @IBOutlet weak var mailButton: UIButton!
    
-    
+    //cool
     
 
     override func viewDidLoad() {
@@ -35,8 +36,15 @@ class ViewController: UIViewController {
         messageLabel.text = enterMessageTextField.text
         messageLabel.textColor = UIColor.redColor()
         enterMessageTextField.text = ""
-        enterMessageTextField.resignFirstResponder()
         
+        nameLabel.hidden = false
+        nameLabel.text = enterNameTextField.text
+        nameLabel.textColor = UIColor.blueColor()
+        enterNameTextField.text = ""
+        
+        enterNameTextField.resignFirstResponder()
+        
+        enterMessageTextField.resignFirstResponder()
         mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
     }
 
